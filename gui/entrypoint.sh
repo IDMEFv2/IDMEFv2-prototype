3c463eae86f6bd56a187eb721f7ac535e4e0a41f
Maxime @ Bytes 'n AI <maxime@bytesnai.tech>
2024-08-11 23:10:28 +0000

    GUI: change default postgres password

--- gui/entrypoint.sh
+++ gui/entrypoint.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 echo "Waiting for database to be ready ..."
 while true; do
-    pg_isready -U postgres -h postgres
+    pg_isready -U prelude -h postgres
     if [ $? -eq 0 ]; then
         break
     else
@@ -24,7 +24,7 @@ echo "Running GUI"
 sleep 20
 /usr/local/bin/prewikka-cli -C 'sync plugin'
 
-PGPASSWORD=changeme psql -d prewikka -h postgres -U postgres -c "UPDATE prewikka_user_configuration SET config = '{\"\": {\"password\": \"\$5\$rounds=535000\$F0fsipTsXZvzOM6n\$r4D77J5PrWT/ZfRx7z0D/44d64irmg5ENnEwvLOASW.\"}}' WHERE userid = '21232f297a57a5a743894a0e4a801fc3';" || :
+PGPASSWORD=prelude psql -d prewikka -h postgres -U prelude -c "UPDATE prewikka_user_configuration SET config = '{\"\": {\"password\": \"\$5\$rounds=535000\$F0fsipTsXZvzOM6n\$r4D77J5PrWT/ZfRx7z0D/44d64irmg5ENnEwvLOASW.\"}}' WHERE userid = '21232f297a57a5a743894a0e4a801fc3';" || :
 echo "Webinterface's admin's password sets to S4Sadmin"
 
 /usr/local/bin/prewikka-httpd -p 80
